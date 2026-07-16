a7f9a98226b51f2201b57fa62db607c1187bbca5
Anton Sokolov <sokolov.anton.ev@gmail.com>
2026-07-16 20:29:46 +0200

    Remove GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://asokolov-ua.github.io",
-  base: "/Patriotic-Youth-Of-Ukraine-AstroProject",
-
   vite: {
     plugins: [tailwindcss()],
   },
